b835764e491ef8f7faf15ce0dfc2cd537025af82
danillukin161100 <danilluikin161100@gmail.com>
2023-09-15 17:28:35 +0300

    apiFilters

--- wp-content/api/service/filters/index.php
+++ wp-content/api/service/filters/index.php
@@ -3,9 +3,14 @@ header('Content-Type: application/json');
 require_once $_SERVER['DOCUMENT_ROOT'] . '/wp-load.php';
 
 $data = file_get_contents('php://input');
-$data = json_decode($data);
+// $data = json_decode($data);
+$data = [
+	'city' => 6,
+	'cat' => 577,
+	'firm' => 175,
+];
 $result = [
-	'response' => false,
+	'success' => false,
 	'message' => 'Что-то пошло не так',
 ];
 
@@ -14,6 +19,51 @@ if (empty($data)) {
 	echo json_encode($result, 320);
 }
 
-$result['data'] = $data;
+$args = [
+	'post_type' => 'service',
+	'posts_per_page' => -1,
+];
+
+$city_id = $data['city'];
+$city = 'all_cities';
+if (!empty($city_id)) {
+	$args['tax_query'][] = [
+		'taxonomy' => 'city',
+		'field' => 'ID',
+		'terms' => $city_id,
+	];
+	$city = get_term_by('ID', $city_id, 'city');
+}
+
+$cat_id = $data['cat'];
+$cat = 'all_categories';
+if (!empty($data['cat'])) {
+	$args['tax_query'][] = [
+		'taxonomy' => 'categories',
+		'field' => 'ID',
+		'terms' => $data['cat'],
+	];
+	$cat = get_term_by('ID', $cat_id, 'categories');
+}
+
+$firm_id = $data['firm'];
+$firm = 'all_firms';
+if (!empty($data['firm'])) {
+	$args['tax_query'][] = [
+		'taxonomy' => 'firms',
+		'field' => 'ID',
+		'terms' => $data['firm'],
+	];
+	$firm = get_term_by('ID', $firm_id, 'firms');
+}
+
+$services = new WP_Query($args);
+
+if ($services->have_posts()) {
+	$result['success'] = true;
+	$result['count'] = count($services->posts);
+	$result['link'] = get_home_url() . '/services/' . $city->slug . '/' . $cat->slug . '/' . $firm->slug;
+	unset($result['message']);
+}
 
 echo json_encode($result, 320);
