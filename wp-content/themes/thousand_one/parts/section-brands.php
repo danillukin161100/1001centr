657f4f5c51d70a8749812106275f05f716da1b8e
danillukin161100 <danilluikin161100@gmail.com>
2023-09-29 10:54:24 +0300

    asdasd

--- wp-content/themes/thousand_one/parts/section-brands.php
+++ wp-content/themes/thousand_one/parts/section-brands.php
@@ -1,7 +1,7 @@
 <?php
 $firms = get_terms([
 	'taxonomy' => 'firms',
-	'hide_empty' => false,
+	// 'hide_empty' => false,
 ]);
 $iteration = (isset($_GET['firm_iteration'])) ? $_GET['firm_iteration'] : 1;
 $iteration--;
@@ -21,8 +21,7 @@ if (empty($firms)) return false;
 				<a href="<?= getFirmsPermalink($firm) ?>" class="brands__item">
 					<img data-src="<?= getFirmImageName($firm->term_id, 1) ?>" src="<?= kama_thumb_src('w=200&h=120&crop=0', getFirmImageName($firm->term_id)) ?>" alt="">
 				</a>
-				<?php // if ($key >= $end) break; 
-				?>
+				<?php if ($key >= $end) break; ?>
 			<?php } ?>
 		</div>
 		<!-- /.brands-box -->
